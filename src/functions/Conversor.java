ba447d297f424c6c2c364e47854fd95973eb7514
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-05-08 15:51:29 -0300

    Actualizacion del metodo para obtener las monedas disponibles

--- src/functions/Conversor.java
+++ src/functions/Conversor.java
@@ -1,7 +1,13 @@
 package functions;
 
 import api_consume.Consultas;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import models.Moneda;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Conversor {
 
@@ -38,9 +44,25 @@ public class Conversor {
     }
 
     public void obtenerMonedas() {
+        List<Moneda> monedas = new ArrayList<>();
+
         JsonObject respuesta = consulta.realizarConsulta("/codes");
 
         String estado = respuesta.get("result").getAsString();
-        
+        if (estado.compareToIgnoreCase("success") == 0) {
+            JsonArray codigos = respuesta.get("supported_codes").getAsJsonArray();
+
+            for (JsonElement e : codigos) {
+                JsonArray moneda = e.getAsJsonArray();
+                String codigo = moneda.get(0).getAsString();
+                String pais = moneda.get(1).getAsString();
+
+                monedas.add(new Moneda(codigo, pais));
+            }
+
+            for (Moneda m : monedas) {
+                System.out.println(m);
+            }
+        }
     }
 }
